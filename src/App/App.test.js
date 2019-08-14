f72f01538ad1e6610bb0770ec012347e5ce537d9
Travis Rollins <troll59910@aol.com>
2019-08-14 15:34:07 -0600

    Add start up branch for unit testing react

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-describe('App', () => {
-  it('should update state with an idea when addIdea is called', () => {
-    // Setup
-    const wrapper = shallow(<App />);
-    const mockIdea = { title: 'sweaters for pugs', description: 'why not?', id: Date.now() };
-    const expected = [mockIdea];
-
-    // Expectation
-    expect(wrapper.state('ideas')).toEqual([]);
-
-    // Execution
-    wrapper.instance().addIdea(mockIdea);
-
-    // Expectation
-    expect(wrapper.state('ideas')).toEqual(expected);
-  });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './Card.css'
 
-const Card = ({ id, title, description, removeIdea, isFavorite }) => {
-  var favoriteClass = isFavorite ? 'favorite' : 'card'
-
+const Card = ({ id, title, description, removeIdea }) => {
   return (
-    <section className={favoriteClass}>
+    <section className='card'>
       <h3>{ title }</h3>
       <p>{ description }</p>
       <button onClick={() => removeIdea(id)}>🗑</button>
